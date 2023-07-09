4c60837e8841c40e90a5e89a171b554a2c584d24
mohsinsyed729 <136225413+mohsinsyed729@users.noreply.github.com>
2023-07-09 13:22:09 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+     #OPERATORS
+
+#addition
+print(2+3)
+#subtraction
+print(3-2)
+# multiplication
+print(2*3)
+# exponential
+print(2**3)  # 2*2*2
+print(5**3)  # 5*5*5
+# division
+print(6/2)
+# floor division
+print(6//2)
+# modulus
+print(5%3)
+
+
+
+                      #EXERCISE
+                 #CREATING CALCULATOR
+
+a=10
+b=5
+print("the value of", 10, "+", 5, "is:", a+b)
+print("the value of", 10, "-", 5, "is:", a-b)
+print("the value of", 10, "*", 5, "is:", a*b)
+print("the value of", 10, "/", 5, "is:", a/b)
+print("the value of", 10, "**", 5, "is:", a**b)
+
